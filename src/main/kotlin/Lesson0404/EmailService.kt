4ba608c3ae1d8c386b09e2ed7d640ed88e0b809c
Degak <degtiar92@gmail.com>
2023-04-04 22:10:17 +0300

    Lesson 04-04. Полиморфизм на примере MessageSender

---
+++ src/main/kotlin/Lesson0404/EmailService.kt
@@ -0,0 +1,14 @@
+package Lesson0404
+
+class EmailService:MessageService() {
+    override fun sendMessage(sender: String, recever: String, message: String) {
+        if ((sender.contains("@")) && (recever.contains("@"))) {
+            super.sendMessage(sender, recever, message)
+        }
+    }
+
+
+
+
+
+}
